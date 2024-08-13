fc335b6f32669dcb18142e54bd55867321225113
bhargavivennam <51819569+bhargavivennam@users.noreply.github.com>
2024-08-12 20:46:50 -0500

    Added Test Properties

--- src/test/java/com/learnspring/userdetailsapi/UserDetailsApiApplicationTests.java
+++ src/test/java/com/learnspring/userdetailsapi/UserDetailsApiApplicationTests.java
@@ -3,10 +3,12 @@ package com.learnspring.userdetailsapi;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(classes = UserDetailsApiApplication.class)
 @ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class UserDetailsApiApplicationTests {
 
     @Test
